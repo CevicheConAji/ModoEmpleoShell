72e5c5a89c069620015d945db0137e9b01d1a515
cevicheconaji <zavalachirapiero@gmail.com>
2024-10-24 02:30:33 +0200

    Agregar archivos de proyecto.

---
+++ ModoEmpleoShell/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ModoEmpleoShell
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ModoEmpleoShell/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace ModoEmpleoShell
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSeleccionarFormaPago_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSeleccionarCurso_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcularPrecio_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+}
